6ec0fa1a5363a84c4bf8bc342fbcac3befb2ed8f
CloudSalander <ruben.as532@gmail.com>
2026-05-15 14:41:48 +0200

    Refactor on game logic

--- class/Game.php
+++ class/Game.php
@@ -6,7 +6,14 @@ class Game {
     const COLUMNS = 4;
     const NOT_PUSHED = "X";
     const PUSHED = "O";
-
+    const DIRECTIONS = [
+        [0, 0],   
+        [-1, 0],  
+        [1, 0],  
+        [0, 1], 
+        [0, -1]   
+    ];
+    
     private array $boxes;
 
     public function __construct(){
@@ -25,24 +32,23 @@ class Game {
     }
 
     public function pushBox(int $i, int $j): void {
-        //TODO: validate this input
-        //TODO:Obviouslly we have to refactor that
-        if(isset($this->boxes[$i][$j])) $this->boxes[$i][$j] = !$this->boxes[$i][$j];
-        if(isset($this->boxes[$i-1][$j]))  $this->boxes[$i-1][$j] =  !$this->boxes[$i-1][$j];
-        if(isset($this->boxes[$i+1][$j]))  $this->boxes[$i+1][$j] =  !$this->boxes[$i+1][$j];
-        if(isset($this->boxes[$i][$j+1]))  $this->boxes[$i][$j+1] =  !$this->boxes[$i][$j+1];
-        if(isset($this->boxes[$i][$j-1]))  $this->boxes[$i][$j-1] =  !$this->boxes[$i][$j-1];
+        foreach(self::DIRECTIONS as [$x,$y]) {
+            $currentRow =  $i+$x;
+            $currentColumn = $j+$y;
+            if(isset($this->boxes[$currentRow][$currentColumn])) $this->switchBox($currentRow,$currentColumn);
+        }
+
         $this->print();
     }
 
     private function initializeGame(): void {
-        //TODO: Sure we can do this better or less manually 
         for($i = 0; $i < self::ROWS; ++$i) {
-            $this->boxes[$i] = [];
-            for($j = 0; $j < self::COLUMNS; ++$j) {
-                array_push($this->boxes[$i],false);
-            }
+            $this->boxes[$i] = array_fill(0,self::COLUMNS,false);
         }
+    }
+
+    private function switchBox(int $x, int $y): void {
+        $this->boxes[$x][$y] = !$this->boxes[$x][$y];
     }
 }
 ?>
